0759e44e09fba078b8e2d22e27042777ce6bf9eb
Zack <reportzack@gmail.com>
2024-03-14 17:31:09 -0500

    Connection to the database and User Schema creation

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 const CONNECTION_PORT = process.env.PORT || 3001;
 
 app.listen(CONNECTION_PORT, () => {
-    console.log(
-        "listening on " + "http://localhost:" + CONNECTION_PORT + "..."
-    );
+    console.log(`[INDEX] listening on http://localhost: ${CONNECTION_PORT}...`);
+    db_connection();
 });
+
+const db_connection = () => {
+    try {
+        mongoose
+            .connect(process.env.DB_CONNECTION_STRING)
+            .then(() => {
+                console.log(`[INDEX] Connected to MongoDB`);
+            })
+            .catch(() => {
+                console.log("[INDEX] Could not connect to MongoDB");
+            });
+    } catch (error) {
+        console.log(error);
+    }
+};
